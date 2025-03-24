96377e28a7592557c6f45464541b714898ccc999
HatcheYumer <hatceyumer2003@gmail.com>
2025-03-24 13:06:40 +0200

    cezar_bruteforce.c

---
+++ cezar_bruteforce.c
@@ -0,0 +1,34 @@
+
+////domashna dekodirane-dekodirane na shifira na cezar
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAXN 1000
+#define ALPHA_LENGTH 26
+
+
+char *cezar_decipher(char *cipher, int key)
+{
+    int length = strlen(cipher);
+    char *plaintext = (char *)malloc(sizeof(char) * (length + 1));
+    for (int i = 0; i < length; i++)
+    {
+        if (cipher[i] >= 'a' && cipher[i] <= 'z')
+        {
+            plaintext[i] = (cipher[i] - 'a' - key + 26) % 26 + 'a';
+        }
+        else if (cipher[i] >= 'A' && cipher[i] <= 'Z')
+        {
+            plaintext[i] = (cipher[i] - 'A' - key + 26) % 26 + 'A';
+        }
+        else if (cipher[i] >= '0' && cipher[i] <= '9')
+        {
+            plaintext[i] = (cipher[i] - '0' - key + 10) % 10 + '0';
+        }
+    }
+    plaintext[length] = '\0';
+
+    return plaintext;
+}
+int main()
